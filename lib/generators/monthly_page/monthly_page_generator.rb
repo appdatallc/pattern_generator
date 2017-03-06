106ed56b4ee5c87b910c18cbb02332becdfbe283
Scott Johnson <fuzzygroup@gmail.com>
2017-03-06 10:32:13 -0500

    Fixed output directory

--- lib/generators/monthly_page/monthly_page_generator.rb
+++ lib/generators/monthly_page/monthly_page_generator.rb
@@ -4,15 +4,17 @@ class MonthlyPageGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   class_option :test_suite, type: :string, default: 'rspec', desc: 'Test framework to generate test. (rspec or minitest)'
 
+  DESTINATION_PATH = "models"
+
   def copy_monthly_page_file
-    template 'monthly_page.rb', "app/monthly_page/#{file_name}.rb"
+    template 'monthly_page.rb', "app/#{DESTINATION_PATH}/#{file_name}.rb"
   end
 
   def copy_service_test_file
     if options.test_suite == 'rspec'
-      template 'monthly_page_spec.rb', "spec/monthly_page/#{file_name}_spec.rb"
+      template 'monthly_page_spec.rb', "spec/#{DESTINATION_PATH}/#{file_name}_spec.rb"
     elsif options.test_suite == 'minitest'
-      template 'monthly_page_test.rb', "test/monthly_page/#{file_name}_test.rb"
+      template 'monthly_page_test.rb', "test/#{DESTINATION_PATH} /#{file_name}_test.rb"
     end
   end
 
